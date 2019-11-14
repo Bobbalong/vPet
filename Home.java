2e9b39bd5dbf60d93e5472b39f9cc611ec65f667
Bobbalong <bobbalong_666@hotmail.com>
2019-11-14 22:25:37 +0000

    Create Home.java

---
+++ Home.java
@@ -0,0 +1,18 @@
+package vPetSrc;
+
+public class Home {
+	
+	public static int[] toilet = {0, 20};
+	public static int[] bed = {100, 194};	
+	public static int[] trough = {110};	
+
+	public static void areaAssess() {
+		
+	}
+	
+	public static void waste () {
+		
+	}
+	
+	
+}
