1f4ce3dd850356b1aeb37f088495ebeb1d202fa6
Bobbalong <bobbalong_666@hotmail.com>
2019-11-14 22:24:17 +0000

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -1,13 +1,13 @@
 package vPetSrc;
 
-import java.util.*;
-
 public class Menu {
 	
 	public static int bread = 10;
 	public static double breadTick;
-	public static double breadValue;
+	public static double breadValue = 10 * vPet.stomachMod;
     
+	
+	
 	public static void breadCheck() {
 
 		breadValue = 10 * vPet.stomachMod;
