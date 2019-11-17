eb97e47b5f55afaa420bf3fbd4eedb32cfbd1816
Bobbalong <bobbalong_666@hotmail.com>
2019-11-17 22:07:38 +0000

    Update Home.java

--- Home.java
+++ Home.java
@@ -3,8 +3,10 @@ package vPetSrc;
 public class Home {
 	
 	public static int[] toilet = {0, 20};
-	public static int[] bed = {100, 194};	
-	public static int[] trough = {110};	
+	public static int[] bed = {100, 194};
+	
+	public static int[] bowl = {164, 216, 0};
+	
 
 	public static void areaAssess() {
 		
